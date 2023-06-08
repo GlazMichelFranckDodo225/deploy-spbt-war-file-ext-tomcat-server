42d6eea11be2c7474323ff5c876a5a7a8606e77b
FranckDodo91 <devlogappdgmf@gmail.com>
2023-06-08 17:33:10 +0200

    Project initialization

---
+++ src/test/java/com/dgmf/deployspbtwarfileexttomcatserver/DeploySpbtWarFileExtTomcatServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgmf.deployspbtwarfileexttomcatserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeploySpbtWarFileExtTomcatServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
